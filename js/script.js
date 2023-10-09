d21b9c9f6d43f0bce81179224a54d05e877d9d1d
Cesar Gabriel <cesargabrielsousasousa196@gmail.com>
2023-10-09 15:45:48 -0300

    adição do frame

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ GameItems.forEach(GameItem => {
         e.target.classList.add('Active')
     })
 })
+
+
+var User = JSON.parse(localStorage.getItem("UserData")) || null
+
+if (User == null) {
+    document.querySelector(".links").innerHTML = `
+    <a onclick="LoadPage('html/login.html')"  title="Minha Carteira" class="nav-link slide-nav-link" >
+        Entrar
+    </a>
+    <a href="" title="Minha Carteira" class="Carteira nav-link slide-nav-link" >
+        Cadastrar-se
+    </a>
+    `
+}else {
+    document.querySelector(".links").innerHTML = `
+    <a href="" title="Minha Carteira" class=" nav-link slide-nav-link" >
+        <span class="material-symbols-outlined">account_balance_wallet</span>
+    </a>
+    `
+}
+
+
+function LoadPage(page) {
+    document.querySelector("#Frame").src = page
+}
